f52b35bb6f9af2866580fd316d7b2c221869b03b
Noah Stiltner <nstilt1@lsu.edu>
2024-05-21 16:01:04 -0500

    add create_deployment_script.sh

---
+++ scripting/create_deployment_script.sh
@@ -0,0 +1,31 @@
+# store the name of the lambda:
+name=$1
+# store the IAM number
+iam=$2
+
+# navigate to directory to add the new function
+cd ..
+
+# navigate to new directory
+cd $name
+
+# create builds directory
+mkdir builds
+
+# create deploy.sh
+cat << EOF > deploy.sh
+# find latest build file
+cd builds
+filename=\$(find . -maxdepth 1 -type f -printf "%f\n" | sort | tail -n 1)
+
+aws lambda create-function --function-name ${name} \\
+--handler bootstrap \\
+--zip-file fileb://./\$filename \\
+--runtime provided.al2 \\
+--role arn:aws:iam::${iam} for the Lambda Function \\
+--region us-east-1 \\
+--architectures arm64
+EOF
+
+# make it executable
+sudo chmod +x ./deploy.sh
